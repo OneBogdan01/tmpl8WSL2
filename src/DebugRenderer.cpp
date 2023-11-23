ec9afc330d3a8eff7d42945ba45c00f480feee9f
Bogdan Mocanu <bogdan.game.development@gmail.com>
2023-11-23 10:16:03 +0100

    ARM working

--- src/DebugRenderer.cpp
+++ src/DebugRenderer.cpp
@@ -1,10 +1,64 @@
 ﻿#include "DebugRenderer.h"
-#include <GLES2/gl2.h>
+#include "template.h"
 
+
+DebugRenderer::DebugRenderer()
+{
+	simpleShader = new Shader(
+		"shaders/BasicVertexShader.vert",
+		"shaders/BasicFragmentShader.frag");
+}
 
 void DebugRenderer::drawLine(const btVector3& from, const btVector3& to, const btVector3& color)
 {
-	glColor4f(color.x(), color.y(), color.z(), 1.0f);
-	glVertex3f(from.x(), from.y(), from.z());
-	glVertex3f(to.x(), to.y(), to.z());
+	simpleShader->Bind();
+	// Use modern OpenGL ES 3.1 functions
+	GLuint vertexArray;
+	glGenVertexArrays(1, &vertexArray);
+	glBindVertexArray(vertexArray);
+
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+
+	GLfloat vertices[] = {
+		from.x(), from.y(), from.z(),
+		to.x(), to.y(), to.z()
+	};
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Assuming you have a shader program with a "color" attribute
+	simpleShader->SetFloat3("color", color.x(), color.y(), color.z());
+
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+
+	glDrawArrays(GL_LINES, 0, 2);
+
+	glDeleteVertexArrays(1, &vertexArray);
+	glDeleteBuffers(1, &vertexBuffer);
+	simpleShader->Unbind();
+}
+
+void DebugRenderer::drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance,
+                                     int lifeTime, const btVector3& color)
+{
+}
+
+void DebugRenderer::reportErrorWarning(const char* warningString)
+{
+}
+
+void DebugRenderer::draw3dText(const btVector3& location, const char* textString)
+{
+}
+
+void DebugRenderer::setDebugMode(int debugMode)
+{
+}
+
+int DebugRenderer::getDebugMode() const
+{
 }
--- src/DebugRenderer.h
+++ src/DebugRenderer.h
@@ -1,9 +1,13 @@
 ﻿#pragma once
 #include <LinearMath/btIDebugDraw.h>
 
+
+class Shader;
+
 class DebugRenderer : public btIDebugDraw
 {
 public:
+	DebugRenderer();
 	void drawLine(const btVector3& from, const btVector3& to, const btVector3& color) override;
 	void drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance, int lifeTime,
 	                      const btVector3& color) override;
@@ -11,4 +15,7 @@ public:
 	void draw3dText(const btVector3& location, const char* textString) override;
 	void setDebugMode(int debugMode) override;
 	int getDebugMode() const override;
+
+private:
+	Shader* simpleShader = nullptr;
 };
