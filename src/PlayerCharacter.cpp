8a9810db1c366d4c00604e6edfc76321fb47bbdd
Bogdan Mocanu <bogdan.game.development@gmail.com>
2023-12-11 10:23:38 +0100

    save

--- src/PlayerCharacter.cpp
+++ src/PlayerCharacter.cpp
@@ -7,118 +7,120 @@
 
 btTransform PlayerCharacter::SetPositionTransform(const btVector3& startingPosition)
 {
-	btTransform playerTransform;
+    btTransform playerTransform;
 
-	playerTransform.setIdentity();
-	playerTransform.setOrigin(startingPosition);
-	return playerTransform;
+    playerTransform.setIdentity();
+    playerTransform.setOrigin(startingPosition);
+    return playerTransform;
 }
 
 PlayerCharacter::PlayerCharacter(btDiscreteDynamicsWorld* dynamicsWorld, const btVector3& startingPosition)
 {
-	const btTransform playerTransform = SetPositionTransform(startingPosition);
+    inputManager = &Game::GetInputManager();
 
-	//get the mesh info
-	//playerModel = new SkinnedMesh();
-	//playerModel->LoadMesh("assets/Run.dae");
-	//debug
-	/*UtilityAssimp util;
-	util.PrintMeshInformation("assets/Run.dae");*/
-	//create collision shape
-	StaticModel staticPlayerMode("assets/excalibur/tris.md2", true);
+    const btTransform playerTransform = SetPositionTransform(startingPosition);
 
-	btConvexShape* collider = World::CreateBoundingBoxModel(staticPlayerMode.GetMeshes(), 0.1f);
-	// Create a ghost object for collision detection
-	ghostObject = new btPairCachingGhostObject();
-	ghostObject->setCollisionShape(collider);
-	ghostObject->setWorldTransform(playerTransform);
-	ghostObject->setCollisionFlags(btCollisionObject::CF_CHARACTER_OBJECT);
-	dynamicsWorld->getBroadphase()->getOverlappingPairCache()->setInternalGhostPairCallback(new btGhostPairCallback());
-	ghostObject->setUserPointer(&gameObject);
-	characterController = new btKinematicCharacterController(ghostObject, collider, 0.05f);
-	characterController->setGravity(dynamicsWorld->getGravity());
+    //get the mesh info
+    //playerModel = new SkinnedMesh();
+    //playerModel->LoadMesh("assets/Run.dae");
+    //debug
+    /*UtilityAssimp util;
+    util.PrintMeshInformation("assets/Run.dae");*/
+    //create collision shape
+    StaticModel staticPlayerMode("assets/excalibur/tris.md2", true);
 
-	dynamicsWorld->addCollisionObject(ghostObject, btBroadphaseProxy::CharacterFilter,
-	                                  btBroadphaseProxy::AllFilter);
-	dynamicsWorld->addAction(characterController);
+    btConvexShape* collider = World::CreateBoundingBoxModel(staticPlayerMode.GetMeshes(), 0.1f);
+    // Create a ghost object for collision detection
+    playerCharacterGhost = new btPairCachingGhostObject();
+    playerCharacterGhost->setCollisionShape(collider);
+    playerCharacterGhost->setWorldTransform(playerTransform);
+    playerCharacterGhost->setCollisionFlags(btCollisionObject::CF_CHARACTER_OBJECT);
+    dynamicsWorld->getBroadphase()->getOverlappingPairCache()->setInternalGhostPairCallback(new btGhostPairCallback());
+    playerCharacterGhost->setUserPointer(&gameObject);
+    characterController = new btKinematicCharacterController(playerCharacterGhost, collider, 0.05f);
+    characterController->setGravity(dynamicsWorld->getGravity());
 
-	characterController->setMaxJumpHeight(3.0f);
-	///set them sometime
-	characterController->setFallSpeed(5.0f);
-	characterController->setJumpSpeed(14.0f);
-	originalTransform = characterController->getGhostObject()->getWorldTransform();
+    dynamicsWorld->addCollisionObject(playerCharacterGhost, btBroadphaseProxy::CharacterFilter,
+                                      btBroadphaseProxy::AllFilter);
+    dynamicsWorld->addAction(characterController);
 
-	shader = new Shader(
-		"shaders/ModelLoading.vert",
-		"shaders/ModelLoading.frag");
-	shader->Bind();
-	shader->SetFloat3("lightPos", Game::GetLightPos().x, Game::GetLightPos().y, Game::GetLightPos().z);
-	shader->SetFloat3("material.specular", 0.5f, 0.5f, 0.5f);
-	shader->SetFloat("material.shininess", 32.0f);
-	shader->SetFloat3("light.ambient", 0.2f, 0.2f, 0.2f);
-	shader->SetFloat3("light.diffuse", 0.5f, 0.5f, 0.5f); // darken diffuse light a bit
-	shader->SetFloat3("light.specular", 1.0f, 1.0f, 1.0f);
-	shader->SetInt("material.diffuse", 0);
-	shader->SetFloat3("objectColor", 1.0f, 0.5f, 0.0f);
-	shader->SetFloat3("lightColor", 1.0f, 1.0f, 1.0f);
+    characterController->setMaxJumpHeight(3.0f);
+    ///set them sometime
+    characterController->setFallSpeed(5.0f);
+    characterController->setJumpSpeed(14.0f);
+    originalTransform = characterController->getGhostObject()->getWorldTransform();
 
-	/*for (unsigned int i = 0; i < ARRAY_SIZE_IN_ELEMENTS(m_boneLocation); i++)
-	{
-		char Name[128];
-		memset(Name, 0, sizeof(Name));
-		SNPRINTF(Name, sizeof(Name), "gBones[%d]", i);
-		GLuint Location = glGetUniformLocation(shader->ID, Name);
+    shader = new Shader(
+        "shaders/ModelLoading.vert",
+        "shaders/ModelLoading.frag");
+    shader->Bind();
+    shader->SetFloat3("lightPos", Game::GetLightPos().x, Game::GetLightPos().y, Game::GetLightPos().z);
+    shader->SetFloat3("material.specular", 0.5f, 0.5f, 0.5f);
+    shader->SetFloat("material.shininess", 32.0f);
+    shader->SetFloat3("light.ambient", 0.2f, 0.2f, 0.2f);
+    shader->SetFloat3("light.diffuse", 0.5f, 0.5f, 0.5f); // darken diffuse light a bit
+    shader->SetFloat3("light.specular", 1.0f, 1.0f, 1.0f);
+    shader->SetInt("material.diffuse", 0);
+    shader->SetFloat3("objectColor", 1.0f, 0.5f, 0.0f);
+    shader->SetFloat3("lightColor", 1.0f, 1.0f, 1.0f);
 
-		if (Location == INVALID_UNIFORM_LOCATION)
-		{
-			fprintf(stderr, "Warning! Unable to get the location of uniform '%s'\n", "gBones");
-		}
-		m_boneLocation[i] = Location;
-		printf("m_boneLocation[%d] = %d\n", i, m_boneLocation[i]);
-	}*/
-	shader->Unbind();
+    /*for (unsigned int i = 0; i < ARRAY_SIZE_IN_ELEMENTS(m_boneLocation); i++)
+    {
+        char Name[128];
+        memset(Name, 0, sizeof(Name));
+        SNPRINTF(Name, sizeof(Name), "gBones[%d]", i);
+        GLuint Location = glGetUniformLocation(shader->ID, Name);
+
+        if (Location == INVALID_UNIFORM_LOCATION)
+        {
+            fprintf(stderr, "Warning! Unable to get the location of uniform '%s'\n", "gBones");
+        }
+        m_boneLocation[i] = Location;
+        printf("m_boneLocation[%d] = %d\n", i, m_boneLocation[i]);
+    }*/
+    shader->Unbind();
 }
 
 PlayerCharacter::~PlayerCharacter()
 {
-	delete shader;
+    delete shader;
 }
 
 void PlayerCharacter::CheckForFall()
 {
-	if (position.y < 0)
-	{
-		//falling
-		ghostObject->setWorldTransform(originalTransform);
-	}
+    if (position.y < 0)
+    {
+        //falling
+        playerCharacterGhost->setWorldTransform(originalTransform);
+    }
 }
 
 void PlayerCharacter::Draw()
 {
-	Camera& cam = Game::GetCamera();
-	const glm::vec3 camPos = Game::GetCameraPosition();
+    Camera& cam = Game::GetCamera();
+    const glm::vec3 camPos = Game::GetCameraPosition();
 
-	glm::mat4 model = GetModelMatrix();
-	position = glm::vec3(model[3]);
-	CheckForFall();
-	shader->Bind();
+    glm::mat4 model = GetModelMatrix();
+    position = glm::vec3(model[3]);
+    CheckForFall();
+    shader->Bind();
 
-	shader->SetMat4x4("view", cam.GetViewMat());
-	shader->SetMat4x4("projection", cam.GetProjectionMat());
-	shader->SetMat4x4("model", model);
-	shader->SetFloat3("viewPos", camPos.x, camPos.y, camPos.z);
+    shader->SetMat4x4("view", cam.GetViewMat());
+    shader->SetMat4x4("projection", cam.GetProjectionMat());
+    shader->SetMat4x4("model", model);
+    shader->SetFloat3("viewPos", camPos.x, camPos.y, camPos.z);
 
-	/*vector<Matrix4f> transforms;
-	float deltaTime = timer.elapsed();
-	playerModel->GetBoneTransforms(deltaTime, transforms);
+    /*vector<Matrix4f> transforms;
+    float deltaTime = timer.elapsed();
+    playerModel->GetBoneTransforms(deltaTime, transforms);
 
-	for (uint i = 0; i < transforms.size(); i++)
-	{
-		SetBoneTransform(i, transforms[i]);
-	}*/
-	shader->Unbind();
-	player.SetPosition(position);
-	player.Draw(renderFrame, 0, interpolation, Game::camera->GetViewMat(), Game::camera->GetProjectionMat());
+    for (uint i = 0; i < transforms.size(); i++)
+    {
+        SetBoneTransform(i, transforms[i]);
+    }*/
+    shader->Unbind();
+    player.SetPosition(position);
+    player.Draw(renderFrame, 0, interpolation, Game::camera->GetViewMat(), Game::camera->GetProjectionMat());
 }
 
 //void PlayerCharacter::SetBoneTransform(uint Index, const Matrix4f& Transform)
@@ -134,78 +136,90 @@ void PlayerCharacter::Draw()
 
 glm::mat4 PlayerCharacter::GetModelMatrix() const
 {
-	const GameValues* values = nullptr;
-	values = new GameValues(characterController->getGhostObject()->getWorldTransform());
+    const GameValues* values = nullptr;
+    values = new GameValues(characterController->getGhostObject()->getWorldTransform());
 
-	btTransform trans;
-	values->getWorldTransform(trans);
-	float mat4[16]{0.0f};
-	trans.getOpenGLMatrix(mat4);
-	delete values;
-	return {
-		mat4[0], mat4[1], mat4[2], mat4[3],
-		mat4[4], mat4[5], mat4[6], mat4[7],
-		mat4[8], mat4[9], mat4[10], mat4[11],
-		mat4[12], mat4[13], mat4[14], mat4[15],
-	};
+    btTransform trans;
+    values->getWorldTransform(trans);
+    float mat4[16]{0.0f};
+    trans.getOpenGLMatrix(mat4);
+    delete values;
+    return {
+        mat4[0], mat4[1], mat4[2], mat4[3],
+        mat4[4], mat4[5], mat4[6], mat4[7],
+        mat4[8], mat4[9], mat4[10], mat4[11],
+        mat4[12], mat4[13], mat4[14], mat4[15],
+    };
 }
 
 void PlayerCharacter::InterpolateFrames(float deltaTime)
 {
-	//animation stuff
-	if (interpolation >= 1.0f)
-	{
-		interpolation = 0.0f;
-		if (renderFrame == endFrame)
-		{
-			renderFrame = startFrame;
-			bufferIndex = 0;
-		}
-		else
-		{
-			renderFrame++;
-			bufferIndex++;
-		}
-	}
-	interpolation += 10.0f * deltaTime;
+    //animation stuff
+    if (interpolation >= 1.0f)
+    {
+        interpolation = 0.0f;
+        if (renderFrame == endFrame)
+        {
+            renderFrame = startFrame;
+            bufferIndex = 0;
+        }
+        else
+        {
+            renderFrame++;
+            bufferIndex++;
+        }
+    }
+    interpolation += 10.0f * deltaTime;
 }
 
 void PlayerCharacter::HandleInput(float deltaTime)
 {
-	KeyboardManager& inputManager = Game::GetInputManager();
-
-	dir = btVector3(0, 0, 0);
-	int dirX = 0;
-	dirX += inputManager.IsPressed(Action::MoveLeft) ? -1 : 0;
-	dirX += inputManager.IsPressed(Action::MoveRight) ? 1 : 0;
-	dir += btVector3(static_cast<float>(dirX), 0, 0);
-	if (dirX != 0)
-	{
-		if (characterController->onGround())
-		{
-			characterController->setWalkDirection(btVector3(dir).normalized() * speed * deltaTime);
-		}
-		else
-		{
-			//maybe slower
-			characterController->setWalkDirection(btVector3(dir).normalized() * speed * deltaTime);
-		}
-	}
-	else
-	{
-		characterController->setWalkDirection(btVector3(0, 0, 0));
-	}
-	if (inputManager.IsJustPressed(Action::Jump))
-		if (characterController->onGround())
-		{
-			characterController->jump();
-		}
+    dir = btVector3(0, 0, 0);
+    int dirX = 0;
+    dirX += inputManager->IsPressed(Action::MoveLeft) ? -1 : 0;
+    dirX += inputManager->IsPressed(Action::MoveRight) ? 1 : 0;
+    dir += btVector3(static_cast<float>(dirX), 0, 0);
 }
 
 
+void PlayerCharacter::MoveCharacter(float deltaTime)
+{
+    if (dir != btVector3(0, 0, 0))
+    {
+        if (characterController->onGround())
+        {
+            characterController->setWalkDirection(btVector3(dir).normalized() * speed * deltaTime);
+        }
+        else
+        {
+            //maybe slower
+            characterController->setWalkDirection(btVector3(dir).normalized() * jumpSpeed * deltaTime);
+        }
+    }
+    else
+    {
+        characterController->setWalkDirection(btVector3(0, 0, 0));
+    }
+    if (inputManager->IsJustPressed(Action::Jump))
+        if (characterController->onGround())
+        {
+            characterController->jump();
+        }
+    //Clamp movement
+    btTransform currentTransform = playerCharacterGhost->getWorldTransform();
+    btVector3 currentPosition = currentTransform.getOrigin();
+
+    // Clamp the X position within the specified range
+    currentPosition.setX(btClamped(currentPosition.getX(), minX, maxX));
+
+    // Update the player's position
+    currentTransform.setOrigin(currentPosition);
+    playerCharacterGhost->setWorldTransform(currentTransform);
+}
+
 void PlayerCharacter::Update(float deltaTime)
 {
-	HandleInput(deltaTime);
-
-	InterpolateFrames(deltaTime);
+    HandleInput(deltaTime);
+    MoveCharacter(deltaTime);
+    InterpolateFrames(deltaTime);
 }
