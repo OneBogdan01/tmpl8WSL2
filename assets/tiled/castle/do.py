55bfc6bfb042325d2fd250d79a1fa614bab42f31
Bogdan Mocanu <bogdan.game.development@gmail.com>
2023-12-19 11:25:08 +0100

    save

--- assets/tiled/castle/do.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def rename_files():
-    for filename in os.listdir('.'):
-        if '_' in filename:
-            new_filename = filename.replace('_', '')
-            os.rename(filename, new_filename)
-
-rename_files()
